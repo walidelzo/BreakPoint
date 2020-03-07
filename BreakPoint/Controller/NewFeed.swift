0fff19784a7916c4659d8d2fcf55e03a7d5212c9
walidelzo <walidelzo@gmail.com>
2020-03-07 10:40:45 +0200

    add some features

---
+++ BreakPoint/Controller/NewFeed.swift
@@ -0,0 +1,30 @@
+//
+//  NewFeed.swift
+//  BreakPoint
+//
+//  Created by walid elzo on 1/27/20.
+//  Copyright © 2020 NanoSoft. All rights reserved.
+//
+
+import UIKit
+
+class NewFeed: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
